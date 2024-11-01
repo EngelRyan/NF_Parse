e4f243961cac374f28a4f61b01d02ed3e5801ed6
engelryan <ryaneengel@gmail.com>
2024-11-01 15:37:58 -0300

    modularizing base.py and app.py

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-import subprocess
+from base import extract_first_occurrence, extract_all_occurrences
+import os
+from PyPDF2 import PdfReader, PdfWriter
 
-# Lista de scripts a serem executados
-scripts = ['stok.py', 'fort.py', 'asun.py']
 
-# Executa cada script
-for script in scripts:
-    subprocess.run(['python', script], check=True)
+def main():
+    
+    folder = 'C:\\Users\\Estudante\\Desktop\\SheetParseFlow\\Main\\nfs'
+
+    # List of specific codes for STOK
+    stok_codes = ['42795', '31716', '31717', '31719', '31721', '43200', '43199', '54670', '31712']
+
+    # List of specific codes for FORT
+    fort_codes = ['41867', '45999', '41868']
+
+    # List of specific codes for ASUN
+    asun_codes = [
+        '34405', '34407', '34408', '34411', '34458', '34478', '34479',
+        '34481', '34482', '34492', '34496', '34497', '34498', '34499',
+        '34500', '34501', '34502', '34503', '34505', '34506', '34507',
+        '34508', '34510', '34512', '34513', '35475', '46828', '35975'
+    ]
+
+   
+    # Extract only the first occurrence for each category
+    for file in os.listdir(folder):
+        pdf_path = os.path.join(folder, file)
+        extract_first_occurrence('STOK', stok_codes, pdf_path)
+        extract_first_occurrence('FORT', fort_codes, pdf_path)
+        extract_first_occurrence('ASUN', asun_codes, pdf_path)
+
+if __name__ == '__main__':
+    main()
---
+++ base.py
@@ -0,0 +1,90 @@
+import os
+import re
+from PyPDF2 import PdfReader, PdfWriter
+
+# Path to the folders
+folder = 'C:\\Users\\Estudante\\Desktop\\SheetParseFlow\\Main\\nfs'
+
+# Function to extract the issue date from the text
+def extract_issue_date(text):
+
+    match = re.search(r'RS\s*(\d{2}-\d{2}-\d{4})', text)
+
+    return match.group(1) if match else 'unknown_date'
+
+# Function to generate a unique filename
+def generate_unique_name(base_name):
+
+    index = 1
+    name, extension = os.path.splitext(base_name)
+    unique_name = name
+
+    while os.path.exists(unique_name + extension):
+
+        unique_name = f"{name}({index})"
+        index += 1
+
+    return unique_name + extension
+
+# Function to save the pages to a PDF file
+def save_pdf(writer, name, issue_date):
+
+    if len(writer.pages) > 0:
+
+        pdf_base_name = os.path.join(folder, f'{name} NF_{issue_date}.pdf')
+        pdf_final_name = generate_unique_name(pdf_base_name)
+
+        with open(pdf_final_name, 'wb') as new_pdf:
+            writer.write(new_pdf)
+
+        print(f'Pages saved as {pdf_final_name}')
+
+
+# Function to extract only the first occurrence of specified codes from the PDF
+def extract_first_occurrence(name, codes, pdf_path):
+
+    reader = PdfReader(pdf_path)
+    found_notes = set()  # Set to store already found codes
+
+    for page in reader.pages:
+
+        text = page.extract_text()
+        match = re.search(r'(\d{1,3}(?:\.\d{3})*)\s*RECEBEMOS', text)
+        print(match)
+
+        # Check if any specific code is present on the page
+        for code in codes:
+
+            if code in text and match not in found_notes:
+                print(found_notes)
+
+                writer = PdfWriter()
+                writer.add_page(page)
+                found_notes.add(match)  
+                issue_date = extract_issue_date(text)
+
+                save_pdf(writer, name, issue_date)
+
+                break  
+
+    save_pdf(writer, name, issue_date)
+
+# Function to extract all occurrences of specified codes from the PDF
+def extract_all_occurrences(name, codes, pdf_path):
+
+    reader = PdfReader(pdf_path)
+    writer = PdfWriter()
+    issue_date = 'unknown_date'  
+
+
+    for page in reader.pages:
+        text = page.extract_text()
+
+        for code in codes:
+
+            if code in text:
+
+                writer.add_page(page)
+                issue_date = extract_issue_date(text)
+
+    save_pdf(writer, name, issue_date)
